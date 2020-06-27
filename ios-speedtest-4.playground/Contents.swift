b1aa2d8de31dcf255ca4ea872efc674ab468424b
naser <bvif8n@gmail.com>
2020-06-27 17:10:29 +0300

    speedtest / done

--- ios-speedtest-4.playground/Contents.swift
+++ ios-speedtest-4.playground/Contents.swift
@@ -8,25 +8,33 @@ import UIKit
 
 
 
-
+for i in 1...5 {
+    
+    print("ios")
+}
 //: # 2️⃣ function
 
 // MARK: هذي الداله تقوم بضرب رقمين و ترجع الناتج
 
-func Multiplication(  number1 : Int ,  number2 : Int)
+func Multiplication(  number1 : Int ,  number2 : Int) -> Int
 {
-    
+    number1 * number2
     return number1 * number2
 }
 // MARK:   بما يناسب صيغه هذا الاستدعاء Multiplication قم بتعديل  الداله
 
-Multiplication(5,7)
+Multiplication ( number1: 5, number2: 7)
 
 
 
 
 //: # 3️⃣ struct
-// struct...
+struct Student {
+    var name : String
+    var age : Int
+    var hobbies : [String]
+    
+}
 
 
 
